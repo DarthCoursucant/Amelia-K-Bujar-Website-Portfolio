ee8126f61fb9b580adc9918633461f49aa888410
AKB <Ameliakingabujar2002@gmail.com>
2025-01-09 13:42:20 +0100

    Delete certificate-page.js

--- js-files/certificate-page.js
+++
@@ -1,8 +0,0 @@
-function showContainer(containerId) {
-    var containers = document.getElementsByClassName('container');
-    for (var i = 0; i < containers.length; i++) {
-      containers[i].style.display = 'none';
-    }
-    var containerToShow = document.getElementById(containerId);
-    containerToShow.style.display = 'block';
-  }
